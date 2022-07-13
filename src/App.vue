77a43bf2015eb2c678ec30b9d140e193dabe4ad6
kelly227800 <2529303733@qq.com>
2022-07-13 12:12:10 +0800

    完成了项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>黑马头条</h1>
+    <i class="toutiao toutiao-dianzan"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="login"></div>
   </div>
 </template>
 
@@ -12,4 +19,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.login {
+  width: 694px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 10px;
+}
+</style>
